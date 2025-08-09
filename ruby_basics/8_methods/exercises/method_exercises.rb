5d2a712e6b0e2559e610edd42f032af004df93ab
Christian Eric Cosca <ccosca@iripple.com>
2025-08-09 17:12:01 +0800

    Complete 21st century exercise

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -67,4 +67,7 @@ end
 # parameter: year (an integer)
 # return value: true if the year is between 2001 - 2100, otherwise return false
 # hint: use Comparable#between?
+def twenty_first_century?(year)
+  year.between?(2001, 2100)
+end
 
