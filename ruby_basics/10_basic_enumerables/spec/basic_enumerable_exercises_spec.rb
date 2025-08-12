c32dda8ec92323b6ead790dda40a04d3d05488f3
Christian Eric Cosca <ccosca@iripple.com>
2025-08-12 19:06:40 +0800

    Complete current inventory exercise

--- ruby_basics/10_basic_enumerables/spec/basic_enumerable_exercises_spec.rb
+++ ruby_basics/10_basic_enumerables/spec/basic_enumerable_exercises_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'Basic Enumerable Exercises' do
     end
 
     # remove the 'x' from the line below to unskip the test
-    xit 'outputs item without quantity when value is nil' do
+    it 'outputs item without quantity when value is nil' do
       expect($stdout).to receive(:puts).with("pineapples, quantity: ")
       fruit = { pineapples: nil }
       display_current_inventory(fruit)
