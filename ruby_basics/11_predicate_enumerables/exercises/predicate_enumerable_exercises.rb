b0e8300f688ce348e7bd680d8f683e86a3c48cb1
Christian Eric Cosca <ccosca@iripple.com>
2025-08-12 20:25:44 +0800

    Complete valid scores exercise

--- ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
+++ ruby_basics/11_predicate_enumerables/exercises/predicate_enumerable_exercises.rb
@@ -20,4 +20,5 @@ end
 
 def valid_scores?(score_list, perfect_score)
   # use #one? to return true when only one value in the score_list (hash) is equal to the perfect_score (number)
+  score_list.one? {|score, value| value == perfect_score}
 end
