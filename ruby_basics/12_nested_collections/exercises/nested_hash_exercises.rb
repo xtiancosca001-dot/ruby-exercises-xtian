a7e7fed00cf9c42878aedf1d04c05c4b69328be2
Christian Eric Cosca <ccosca@iripple.com>
2025-08-18 07:57:39 +0800

    Complete add information about language exercise

--- ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
@@ -12,7 +12,9 @@ end
 
 def add_information_about_language(languages, language_name, info_key, info_value)
   # Take languages and add the key/value pair info_key/info_value to the nested
+  languages[language_name][info_key] = info_value
   # hash of language_name, then return the updated languages hash
+  languages
 end
 
 def add_language(languages, language_name, language_info_value)
